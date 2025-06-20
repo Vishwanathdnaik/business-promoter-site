12f1a65438a043ae6fb3bb92155604528491e022
Vishwanath D N <writetous@businesspromoter.co.in>
2025-06-20 13:22:12 +0530

    Fix: use 0.0.0.0 and Render port

--- form_handler.py
+++ form_handler.py
@@ -37,4 +37,6 @@ def submit_form():
         return "Something went wrong.", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
