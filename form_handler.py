23a3135b0054887747a29f6cd29cc94027b8f533
Vishwanathdnaik <157128997+Vishwanathdnaik@users.noreply.github.com>
2025-06-23 21:34:14 +0530

    Update form_handler.py

--- form_handler.py
+++ form_handler.py
@@ -39,3 +39,7 @@ def submit():
     except Exception as e:
         print("❌ ERROR in /submit:", str(e))
         return "Something went wrong.", 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
